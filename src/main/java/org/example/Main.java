e28a8d5ceee3fa203356ab70820fd68ba28b438d
Ayşegül Umay Uçar <umyucr@hotmail.com>
2026-09-09 20:44:26 +0300

    Java ödevleri çözüldü

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,49 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+
+        if (isBarking && (clock >= 20 || clock < 8)) {
+            return true;
+        }
+
+        return false;
     }
 
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (!isSummer && temp >= 25 && temp <= 35) {
+            return true;
+        } else if (isSummer && temp >= 25 && temp <= 45) {
+            return true;
+        }
+
+        return false;
     }
 
     public static double area(double width, double height) {
-       
+       if (width < 0 || height < 0) {
+           return -1;
+       }
+       return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+        return -1;
+    }
+       return Math.PI * radius * radius;
     }
 }
